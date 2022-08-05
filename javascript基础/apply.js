8cc41848d27e39f58938d3a4def81df8cde79321
zhangyue5 <zhangyue5@beisen.com>
2022-08-05 11:12:52 +0800

    apply

---
+++ javascript基础/apply.js
@@ -0,0 +1,13 @@
+Function.prototype.apply = function (obj) {
+    // 调用函数的对象不是函数，报错
+    if (typeof this !== "function") {
+        return new TypeError("Type Error");
+    }
+    // 获取参数
+    let args = arguments[1], result = null;
+    obj = obj ? obj : window; // obj不存在指向window
+    obj.fun = this; // 将函数传给obj的fun函数
+    result = obj.fun(...args); // 调用函数
+    delete obj.fun; // 删除函数
+    return result;
+}
