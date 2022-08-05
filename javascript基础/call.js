b0bb25136e54603d1d029e359f5e5733835151e8
zhangyue5 <zhangyue5@beisen.com>
2022-08-05 11:05:30 +0800

    call

--- javascript基础/call.js
+++ javascript基础/call.js
@@ -7,6 +7,7 @@ Function.prototype.myCall = function (obj) {
     // 获取传入的参数，call方法会直接传入参数
     let args = [...arguments].slice(1);
     let result = null; // 存放执行的结果
+    obj = obj ? obj : window; // obj没有传入，赋值window
     // this是调用该方法的函数，将其赋值给obj的fun方法
     obj.fun = this;
     // obj调用方法，会将方法中的this指向obj
